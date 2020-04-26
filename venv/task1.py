f3e19a86730790da4be12de314100b50020d8b25
pornoiya <49945883+pornoiya@users.noreply.github.com>
2020-04-26 22:54:45 +0500

    made it a bit faster

--- venv/task1.py
+++ venv/task1.py
@@ -8,17 +8,25 @@ import json
 from prettytable import PrettyTable
 
 
-def get_ips_in_traceroute(hostname):
+def get_ips_in_traceroute(hostname, table):
     tracert_ips = []
     traceroute = subprocess.Popen(["tracert", '-w', '100', hostname], stdout=subprocess.PIPE,
                                   stderr=subprocess.STDOUT)
+    iteration = 0
     for line in iter(traceroute.stdout.readline, ""):
         if line:
             ip = re.search('(\d+\.){3}\d+', line.decode('utf8', errors='ignore').strip())
             if not ip:
                 continue
+            iteration += 1
+            current_ip = ip.group()
+            reply = get_as_info_by_ip(current_ip)
+            try:
+                table.add_row([iteration, current_ip, reply['as_number'], reply['as_country_code']])
+            except KeyError:
+                table.add_row([iteration, current_ip, '*', '*'])
             tracert_ips.append(ip.group())
-            print(ip.group())
+            print(f"tracerouting {iteration}/30 ip")
         else:
             break
     return tracert_ips
@@ -36,17 +44,7 @@ if __name__ == '__main__':
         hostname = sys.argv[1]
     except IndexError:
         raise ValueError(f"PLEASE ENTER HOST NAME:\n>> {sys.argv[0].split('/')[-1]} 'www.yourhost.com'")
-    ips = get_ips_in_traceroute(hostname)
     table = PrettyTable()
     table.field_names = ['№', 'IP', 'AS NUMBER', 'COUNTRY']
-    for i in range(1, len(ips) + 1):
-        sys.stdout.write(f'processing ip No {i}\n')
-        sys.stdout.flush()
-        current_ip = ips[i - 1]
-        time.sleep(1)
-        reply = get_as_info_by_ip(current_ip)
-        try:
-            table.add_row([i, current_ip, reply['as_number'], reply['as_country_code']])
-        except KeyError:
-            table.add_row([i, current_ip, '*', '*'])
+    ips = get_ips_in_traceroute(hostname, table)
     print(table)
